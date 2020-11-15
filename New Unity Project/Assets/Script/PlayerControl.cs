79ad247b642aa9791c0fe375161b9db4f6c8cd42
YenChih <tp6u045457@gmail.com>
2020-11-15 17:36:28 +0800

    PlayerControl 加入注釋

--- New Unity Project/Assets/Script/PlayerControl.cs
+++ New Unity Project/Assets/Script/PlayerControl.cs
@@ -31,54 +31,54 @@ public class PlayerControl : MonoBehaviour
     {
         a_timer += Time.deltaTime;
         float xm = 0;
-        if (Input.GetKey(KeyCode.RightArrow)&& ishurt==false)
+        if (Input.GetKey(KeyCode.RightArrow)&& ishurt==false) ///移動
         {
             xm += speed * Time.deltaTime;
         }
         target.Translate(new Vector3(xm, 0, 0));
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))///移動
         {
             xm -= speed * Time.deltaTime;
         }
        
         
-        if (Input.GetKey(KeyCode.Space)&& can_j == true)
+        if (Input.GetKey(KeyCode.Space)&& can_j == true)///跳
         {
             rig.AddForce(new Vector3(0, jumpF, 0), ForceMode.Impulse);
             can_j = false;
             Debug.Log(can_j);
         }
-        if (Input.GetKeyDown(KeyCode.A) && isattack == false)
+        if (Input.GetKeyDown(KeyCode.A) && isattack == false)///攻擊觸發
         {
             Attack();
             a_timer = 0;
         }
-        if (a_timer >= attackcooldown && isattack == true)
+        if (a_timer >= attackcooldown && isattack == true)///攻擊重置
         {
             sword.transform.Rotate(new Vector3(0, 0, 90));
             isattack = false;
         }
         Debug.Log(can_j);
         target.Translate(new Vector3(xm, 0, 0));
-        Debug.Log(a_timer);
+        ///Debug.Log(a_timer);
         
     }
-    private void OnCollisionEnter(Collision collision) {
+    private void OnCollisionEnter(Collision collision) {///判斷是否在地板上
         if (collision.gameObject.tag == "Ground")
         {
             can_j = true;
             ishurt = false;
         }
-        if (collision.gameObject.tag == "Enemy")
+        if (collision.gameObject.tag == "Enemy")///撞到敵人
         {
             rig.AddForce(new Vector3(-hurtX,hurtY, 0), ForceMode.Impulse);
             ishurt = true;
-            Debug.Log("Hurt");
+            ///Debug.Log("Hurt");
         }
     }
-    private void Attack() {
+    private void Attack() {///劍旋轉
         sword.transform.Rotate(new Vector3(0, 0, -90));
-        Debug.Log("Attack");
+///Debug.Log("Attack");
         isattack = true;
        
     }
