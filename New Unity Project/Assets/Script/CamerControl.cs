4027fed7356bd4b52ad301dbeaf6c39004fab0d1
YenChih <tp6u045457@gmail.com>
2020-11-15 14:48:31 +0800

    Camera Control

---
+++ New Unity Project/Assets/Script/CamerControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerControl : MonoBehaviour
+{
+    public GameObject target;
+    private Vector3 offset;
+    public float smooth;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = this.transform.position;
+        position.x = Mathf.Lerp(this.transform.position.x,target.transform.position.x,smooth*Time.deltaTime);
+        this.transform.position = position;
+    }
+}
