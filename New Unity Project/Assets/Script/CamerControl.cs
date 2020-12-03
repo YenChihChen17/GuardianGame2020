9aefee474e53c7b09f8740ade1d5db0cf97698a2
YenChih <tp6u045457@gmail.com>
2020-12-04 00:10:19 +0800

    加入 復活功能, 死亡判斷, GameOver 判斷

--- New Unity Project/Assets/Script/CamerControl.cs
+++ New Unity Project/Assets/Script/CamerControl.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CamerControl : MonoBehaviour
 {
     public GameObject target;
+    public GameObject Home;
     private Vector3 offset;
     public float smooth;
     // Start is called before the first frame update
@@ -15,9 +16,29 @@ public class CamerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 position = this.transform.position;
-        position.x = Mathf.Lerp(this.transform.position.x,target.transform.position.x,smooth*Time.deltaTime);
-        position.y = Mathf.Lerp(this.transform.position.y, target.transform.position.y, smooth * Time.deltaTime);
-        this.transform.position = position;
+        if (GameObject.Find("Player") == true)
+        {
+            target = GameObject.Find("Player");
+            Vector3 position = this.transform.position;
+            position.x = Mathf.Lerp(this.transform.position.x, target.transform.position.x, smooth * Time.deltaTime);
+            position.y = Mathf.Lerp(this.transform.position.y, target.transform.position.y, smooth * Time.deltaTime);
+            this.transform.position = position;
+        }
+        else if(GameObject.Find("Player(Clone)") == true)
+        {
+            target = GameObject.Find("Player(Clone)");
+            Vector3 position = this.transform.position;
+            position.x = Mathf.Lerp(this.transform.position.x, target.transform.position.x, smooth * Time.deltaTime);
+            position.y = Mathf.Lerp(this.transform.position.y, target.transform.position.y, smooth * Time.deltaTime);
+            this.transform.position = position;
+        }
+        else
+        {
+            target = Home;
+            Vector3 position = this.transform.position;
+            position.x = Mathf.Lerp(this.transform.position.x, target.transform.position.x, smooth * Time.deltaTime);
+            position.y = Mathf.Lerp(this.transform.position.y, target.transform.position.y, smooth * Time.deltaTime);
+            this.transform.position = position;
+        }
     }
 }
--- New Unity Project/Assets/Script/GameManeger.cs
+++ New Unity Project/Assets/Script/GameManeger.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManeger : MonoBehaviour
-{
-    static public int PlayerHP = 100;
+{     
+    static public int PlayerHP = 10;
     static public int EnemyHP = 100;
     static public int Damage_P = 5;
     static public int Damage_E = 5;
+
+    public GameObject Player;
+    public GameObject Enemy;
+    public GameObject PlayerP;
+    public GameObject Home;
+
+    public GameObject GameOverUI;
+    public GameObject YouDied;
+
+    public float RebornT;
+    public int CloneNum;
+    private float timer;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +30,42 @@ public class GameManeger : MonoBehaviour
     void Update()
     {
         
+        if (PlayerHP <= 0 && CloneNum >=0) 
+        {
+            if (GameObject.Find("Player") == true)
+            {
+                Destroy(Player);
+            }
+            else
+            {
+                Destroy(GameObject.Find("Player(Clone)"));
+            }
+
+            timer += Time.deltaTime;
+            if (CloneNum>0)
+            {
+                YouDied.SetActive(true);
+                if (timer >= RebornT)
+                {
+                    PlayerHP = 20;
+                    Debug.Log(PlayerHP);
+                    CloneNum = CloneNum - 1;
+                    timer = 0;
+                    YouDied.SetActive(false);
+                    Instantiate(PlayerP,Home.transform.position, new Quaternion(0, 0, 0, 0));
+                }
+            }
+        }
+        if(EnemyHP<=0)
+        {
+            Destroy(Enemy);
+        }
+        if(CloneNum == 0 )
+        {
+            if (GameObject.Find("Player(Clone)") == false)
+            {
+                GameOverUI.SetActive(true);
+            }
+        }
     }
 }
