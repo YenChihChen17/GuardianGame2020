32a76581986fbe6f6f8ba9e67315820e5bf82223
Yen-Ting-Chou <hp10035386@gmail.com>
2021-01-23 00:15:29 +0800

    Update Hurt.cs

--- New Unity Project/Assets/Script/Hurt.cs
+++ New Unity Project/Assets/Script/Hurt.cs
@@ -14,6 +14,6 @@ public class Hurt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.transform.localPosition = new Vector3(1700 * ((float)GameManeger.EnemyHP/(float)FullHP)-1700, 0, 0);
+        this.transform.localPosition = new Vector3(800 * ((float)GameManeger.EnemyHP/(float)FullHP)-800, 0, 0);
     }
 }
