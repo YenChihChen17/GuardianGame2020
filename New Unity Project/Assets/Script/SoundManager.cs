da37e847e84378e3a3bf0f90a0f8ae4a8a6d3753
Eielson.Liao <eielson.liao@aktsk.com>
2021-02-19 17:56:30 +0800

    音效相關置入、音效套用 script

---
+++ New Unity Project/Assets/Script/SoundManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+
+    public AudioSource audioSource;
+    [SerializeField]
+    private AudioClip BossHurtAudio, BossDeadAudio;
+    
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    /*public void HurtAudio()
+    {
+        audioSource.clip = BossHurtAudio;
+        audioSource.play();
+    }
+
+    public void DeadAudio()
+    {
+        audioSource.clip = BossDeadAudio;
+        audioSource.play();
+    }*/
+}
