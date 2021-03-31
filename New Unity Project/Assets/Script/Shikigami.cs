6b4ceb58ce2b6d98d9bcddfbc9df4112d48f0380
Carter Chen <carter.chen@aktsk.com>
2021-03-31 14:51:04 +0800

    式神追尋Code 修正

--- New Unity Project/Assets/Script/Shikigami.cs
+++ New Unity Project/Assets/Script/Shikigami.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class Shikigami : MonoBehaviour
 {
     public static GameObject Target;
+    public GameObject body;
+    public float speed;
     //public static Shikigami instance;
     // Start is called before the first frame update
     void Start()
     {
-        Target = this.gameObject;
+        Target = body.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate((Target.transform.position - this.transform.position)*Time.deltaTime,Space.World);
+        if(Target.transform.position.x - body.transform.position.x > 0)
+        {
+            transform.Translate(new Vector3(speed,0,0)* Time.deltaTime, Space.World);
+        }
+        else if (Target.transform.position.x - body.transform.position.x < 0)
+        {
+            transform.Translate(new Vector3(-speed, 0, 0) * Time.deltaTime, Space.World);
+        }
+        Debug.Log(Target.transform.position.x - this.transform.position.x);
     }
 
 }
